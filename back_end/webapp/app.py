74949cdd0cee23a7bab42e93e5149f760d8468c2
sectum1919 <1048186774@qq.com>
2021-11-22 19:51:13 +0800

    add webapp--rtmp push & flv pull stream & flask route

---
+++ back_end/webapp/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, Response, send_from_directory, session, redirect
+#import cv2
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.after_request
+def add_header(response):
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+@app.route('/stream/<string:file_name>')
+def stream(file_name):
+    session['livepath'] = file_name
+    print("session",session['livepath'])
+    return render_template('live.html', livepath=session['livepath'])
+
+# @app.route('/hls/<string:file_name>')
+# def hls(file_name):
+#     video_dir = '/home/homepages/netpro/m3u8File/'
+#     return redirect(directory=video_dir, filename=file_name)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=12060,debug=True)
